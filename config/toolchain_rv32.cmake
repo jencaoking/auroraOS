d1ca2831f2496db95011dce33d2bb27adf3044e9
jencaoking <663930657@qq.com>
2026-07-24 17:35:02 +0800

    fix(ci): add RISC-V toolchain file for RV32 builds
    - Add config/toolchain_rv32.cmake for cross-compilation
    - Update CI workflow to use toolchain file for RV32 build
    - Fixes '-mabi=ilp32' and '-mcmodel=medany' errors on Windows

---
+++ config/toolchain_rv32.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR RISC-V)
+
+set(CMAKE_C_COMPILER riscv64-unknown-elf-gcc)
+set(CMAKE_CXX_COMPILER riscv64-unknown-elf-g++)
+set(CMAKE_ASM_COMPILER riscv64-unknown-elf-gcc)
+set(CMAKE_AR riscv64-unknown-elf-ar)
+set(CMAKE_OBJCOPY riscv64-unknown-elf-objcopy)
+set(CMAKE_OBJDUMP riscv64-unknown-elf-objdump)
+set(CMAKE_SIZE riscv64-unknown-elf-size)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# RV32 flags: 32-bit RISC-V with Integer, Multiply, Atomic, Compressed extensions
+set(CMAKE_C_FLAGS_INIT "-march=rv32imac -mabi=ilp32 -mcmodel=medany -ffreestanding -fno-builtin -fno-common")
+set(CMAKE_CXX_FLAGS_INIT "-march=rv32imac -mabi=ilp32 -mcmodel=medany -ffreestanding -fno-builtin -fno-common")
